aa84b71d0b6f6d5b4fa9ff6b301806512b690328
Andrea Vibelli <andrea.vibelli@gmail.com>
2020-04-19 15:06:12 +0200

    Add caching on dependencies; restore validation of dependencies; get all dependencies and initialize HashSet

--- rest/src/main/java/org/jboss/pnc/rest/provider/BuildConfigurationProvider.java
+++ rest/src/main/java/org/jboss/pnc/rest/provider/BuildConfigurationProvider.java
@@ -53,6 +53,7 @@ import org.slf4j.LoggerFactory;
 import javax.annotation.security.PermitAll;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
